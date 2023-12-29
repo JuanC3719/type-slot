29eef9b4e268c5e7591a149afec5bfd849975569
juanc3719 <j.c.cuenc1@gmail.com>
2023-12-29 14:22:50 -0800

    adjusted vite config

--- slot-ts-1/vite.config.ts
+++ slot-ts-1/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Specify the output directory for the build artifacts
+  },
+};
